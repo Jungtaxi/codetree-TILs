3afd4443b3402361daf96f7fd620f464342b799e
김정택 <beagentleman7@gmail.com>
2024-05-28 10:21:40 +0900

    Title: 계단 오르기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240528/계단 오르기/climbing-stairs.py
+++ 240528/계단 오르기/climbing-stairs.py
@@ -3,14 +3,13 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 
-dp = [1] *5
+dp = [1] * 3
+dp[0] = 0
 dp[1] = 0
 dp[2] = 1
-dp[3] = 1
-dp[4] = dp[1] + dp[2]
 
-if n > 4:
-    for i in range(5, n+1):
-        dp[i] = dp[i-2] + dp[i-3]
+if n > 2:
+    for i in range(3, n+1):
+        dp.append(dp[i-2] + dp[i-3])
 
 print(dp[n])
