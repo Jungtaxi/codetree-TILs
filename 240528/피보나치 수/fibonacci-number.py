537c4d059d3909aa078124a36eda3a989a87ae28
김정택 <beagentleman7@gmail.com>
2024-05-28 10:11:28 +0900

    Title: 피보나치 수, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240528/피보나치 수/fibonacci-number.py
+++ 240528/피보나치 수/fibonacci-number.py
@@ -1,10 +1,11 @@
+# Tabulation 방법
 import sys
 input = sys.stdin.readline
 n = int(input())
 
 dp = [1]*(n+1)
 if n > 2:
-    for i in range(4, n+1):
+    for i in range(3, n+1):
         dp[i] = dp[i-1] + dp[i-2]
 
 print(dp[n])
