37cb7057e8123c7dc50ab4f826458bb9dbc577c7
김정택 <beagentleman7@gmail.com>
2024-05-29 09:57:46 +0900

    Title: 십자 모양 폭발, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240529/십자 모양 폭발/cross-shape-bomb.py
+++ 240529/십자 모양 폭발/cross-shape-bomb.py
@@ -9,7 +9,7 @@ x, y = x-1, y-1
 bomb = graph[x][y]
 bomb -= 1
 
-def apply_grevity():
+def apply_gravity():
     # 각 열에 대해 아래부터 검사, 비었다면 위의 블럭과 맞바꿉니다.
     for i in range(n):
         for j in range(n-1):
@@ -17,20 +17,17 @@ def apply_grevity():
                 graph[n-j-1][i], graph[n-j-2][i] = graph[n-j-2][i], graph[n-j-1][i]
 
 def remove_by_bomb():
-    if bomb != 0:
-        col_start = 0 if y - bomb < 0 else y - bomb
-        col_end = n-1 if y + bomb >= n else y + bomb
-        row_start = 0 if x - bomb < 0 else x - bomb
-        row_end = n-1 if x + bomb >= n else x + bomb
-
-        for i in range(col_start, col_end+1):
-            graph[x][i] = 0
-        for i in range(row_start, row_end+1):
-            graph[i][y] = 0
+    for i in range(-bomb, bomb + 1):
+        # col 방향
+        if x + i >= 0 and x + i < n :
+            graph[x + i][y] = 0
+        # row 방향
+        if y + i >= 0 and y + i < n :
+            graph[x][y + i] = 0
     
 
 remove_by_bomb()
-apply_grevity()
+apply_gravity()
 
 for i in range(n):
     print(' '.join(map(str, graph[i])))
