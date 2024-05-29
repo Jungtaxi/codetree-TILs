1c85ad4bb94b1a61b255f4a51db980f602d62d9e
김정택 <beagentleman7@gmail.com>
2024-05-29 09:37:01 +0900

    Title: 십자 모양 폭발, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240529/십자 모양 폭발/cross-shape-bomb.py
+++ 240529/십자 모양 폭발/cross-shape-bomb.py
@@ -17,15 +17,16 @@ def apply_grevity():
                 graph[n-j-1][i], graph[n-j-2][i] = graph[n-j-2][i], graph[n-j-1][i]
 
 def remove_by_bomb():
-    col_start = 0 if y - bomb < 0 else y - bomb
-    col_end = n-1 if y + bomb >= n else y + bomb
-    row_start = 0 if x - bomb < 0 else x - bomb
-    row_end = n-1 if x + bomb >= n else x + bomb
+    if bomb != 0:
+        col_start = 0 if y - bomb < 0 else y - bomb
+        col_end = n-1 if y + bomb >= n else y + bomb
+        row_start = 0 if x - bomb < 0 else x - bomb
+        row_end = n-1 if x + bomb >= n else x + bomb
 
-    for i in range(col_start, col_end+1):
-        graph[x][i] = 0
-    for i in range(row_start, row_end+1):
-        graph[i][y] = 0
+        for i in range(col_start, col_end+1):
+            graph[x][i] = 0
+        for i in range(row_start, row_end+1):
+            graph[i][y] = 0
     
 
 remove_by_bomb()
