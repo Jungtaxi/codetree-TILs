b8afc94bde0af894200758b70bb1eaaa7e0c3b41
김정택 <0428kevin@hufs.ac.kr>
2024-06-08 22:55:45 +0900

    Title: 행복한 수열의 개수, Time: 148ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240608/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240608/행복한 수열의 개수/number-of-happy-sequence.py
@@ -3,31 +3,34 @@ from collections import Counter
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-answer = 0
-for i in range(n):
-    tmp = 1
-    for j in range(1, n):
-        if graph[i][j-1] == graph[i][j]:
-            tmp += 1
+if m == 1 :
+    print(n**2)
+else:
+    answer = 0
+    for i in range(n):
+        tmp = 1
+        for j in range(1, n):
+            if graph[i][j-1] == graph[i][j]:
+                tmp += 1
 
-            if tmp >= m :
-                answer += 1
-                break
-        
-        else:
-            tmp = 1
+                if tmp >= m :
+                    answer += 1
+                    break
+            
+            else:
+                tmp = 1
 
-for i in range(n):
-    tmp = 1
-    for j in range(1, n):
-        if graph[j-1][i] == graph[j][i]:
-            tmp += 1
+    for i in range(n):
+        tmp = 1
+        for j in range(1, n):
+            if graph[j-1][i] == graph[j][i]:
+                tmp += 1
 
-            if tmp >= m :
-                answer += 1
-                break
-        
-        else:
-            tmp = 1
+                if tmp >= m :
+                    answer += 1
+                    break
+            
+            else:
+                tmp = 1
 
-print(answer)
+    print(answer)
